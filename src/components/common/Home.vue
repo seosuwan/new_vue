93c730d75bca3ac472e8435cf4e7a0b0e5842fbf
seosuwan <los2ve7@gmail.com>
2021-10-13 14:49:56 +0900

    first commit

---
+++ src/components/common/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>게시판!!!!!!!!!!!!!!!!!!!!!!</h1>
+    <Menu></Menu>
+    <div>
+  
+    </div>
+  </div>
+</template>
+    
+
+<script>
+ 
+import Menu from '../views/Menu.vue';
+export default {
+  name: 'Home',
+  components:{
+    Menu,
+  }
+  
+}
+</script>
+<style scoped>
+
+</style>
---
+++ src/components/views/Menu.vue
@@ -0,0 +1,81 @@
+<template>
+  <n-menu
+    
+    :options="menuOptions"
+    :default-expanded-keys="defaultExpandedKeys"
+    accordion
+  />
+</template>
+
+<script>
+import { defineComponent, h} from 'vue'
+import { NIcon, NMenu } from 'naive-ui'
+import { RouterLink } from 'vue-router'
+import {
+  HomeOutline as HomeIcon,
+  FishOutline as FishIcon,
+  PawOutline as PawIcon,
+  BagOutline as BagOutlineIcon
+} from '@vicons/ionicons5'
+
+function renderIcon (icon) {
+  return () => h(NIcon, null, { default: () => h(icon) })
+  
+}
+
+const menuOptions = [
+   {
+    label: () =>
+      h(
+        RouterLink,
+        {
+          to: {
+            path: '../components/common/Home'
+          }
+        },
+        { default: () => 'Going Home' }
+      ),
+    key: 'go-back-home',
+    icon: renderIcon(HomeIcon)
+  },
+  {
+    label: '헤헤헤',
+    key: 'fish',
+    icon: renderIcon(FishIcon)
+  },
+  {
+    label: 'Bear Paw',
+    key: 'bear-paw',
+    icon: renderIcon(PawIcon),
+    children: [
+      {
+        label: 'Protect Wild Animals',
+        key: 'protect-wild-animals'
+      }
+    ]
+  },
+  {
+    label: 'Both',
+    key: 'both',
+    icon: renderIcon(BagOutlineIcon),
+    children: [
+      {
+        label: "You can't have your cake and eat it",
+        key: 'can-not'
+      }
+    ]
+  }
+]
+
+export default defineComponent({
+    components: {
+        NMenu
+    },
+  setup () {
+    return {
+      menuOptions,
+      defaultExpandedKeys: ['fish', 'braise']
+    }
+  }
+})
+</script>
